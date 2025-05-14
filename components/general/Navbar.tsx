783efe8006853fd81bbb046728e4b37c8a997810
niyooshawebsites <niyooshawebsites@gmail.com>
2025-05-14 16:53:32 +0530

    First commit

---
+++ components/general/Navbar.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+const Navbar = () => {
+  return (
+    <nav className="py-5 flex items-center justify-between">
+      <div className="flex items-center gap-6">
+        <Link href="/">
+          <h1 className="text-3xl semi-bold">
+            Blog<span className="text-blue-500">Marshal</span>
+          </h1>
+        </Link>
+
+        <div className="hidden sm:flex items-center gap-6">
+          <Link
+            href="/"
+            className="text-sm font-medium hover:text-blue-500 transition-colors"
+          >
+            Home
+          </Link>
+          <Link
+            href="/dashboard"
+            className="text-sm font-medium hover:text-blue-500 transition-colors"
+          >
+            Dashboard
+          </Link>
+        </div>
+      </div>
+      <div className="flex items-center gap-4">
+        <Button>Login</Button>
+        <Button variant={"secondary"}>Signup</Button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
